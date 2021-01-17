50e4b557fbcccdf1b527fe167679a83e8a4d9dab
Meghdoot Bansal <Bansal.megh4@gmail.com>
2021-01-17 18:36:28 +0530

    Create StreamShow.js

---
+++ admin/src/components/streams/StreamShow.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { fetchStream } from '../../actions';
+import { connect } from 'react-redux';
+import flv from 'flv.js';
+
+class StreamShow extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
+  componentDidMount(){
+    this.props.fetchStream(this.props.match.params.id);
+    /*this.player = flv.createPlayer({
+      type: flv,
+      url: `http://localhost:8000/live/${this.props.match.params.id}.flv`
+    })
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();*/
+  }
+
+  render(){
+    if(!this.props.stream){
+      return <div>Loading...</div>
+    }
+    return (
+      <div>
+        <video ref={this.videoRef} style={{width: '100%'}} controls={true} />
+        <h1>{this.props.stream.title}</h1>
+        <h5>{this.props.stream.description}</h5>
+      </div>
+    );
+  };
+}
+
+const mapStateToProps= (state, ownProps) => {
+  return {
+    stream : state.streams[ownProps.match.params.id]
+  }
+};
+
+export default connect(mapStateToProps,{ fetchStream })(StreamShow);
